d0c8a60eaf8c825547e0bb1306ea56501fec188e
Scott Lynch <62553406+slteksystemsllc@users.noreply.github.com>
2020-05-12 15:15:49 -0400

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -27,7 +27,11 @@ cp -r /opt/elastic_stack/logstash/logstash_configs/* /opt/logstash_configs/
 mkdir -p /etc/nsm/rules/
 cp -r /opt/elastic_stack/logstash/rules/* /etc/nsm/rules/
 
-mkdir /usr/share/logstash/data/ 
+mkdir /usr/share/logstash/data/
+
+mkdir /opt/domain_stats2/data
+
+sudo chown 1000:1000 /opt/domain_stats2
 
 cd /opt/elastic_stack
 
