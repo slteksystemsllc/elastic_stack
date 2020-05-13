cdab9732a11692ef9150bc94bd890d3419108898
Scott Lynch <62553406+slteksystemsllc@users.noreply.github.com>
2020-05-13 10:05:42 -0400

    Update startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
 sudo apt-get install -y wget
-
 sudo wget https://github.com/slteksystemsllc/elastic_stack/raw/master/scripts/prereq.sh
-
 sudo bash prereq.sh
-
 sudo wget https://github.com/slteksystemsllc/elastic_stack/raw/master/scripts/initialize.sh
-
 sudo bash initialize.sh
-
 cd /opt/elastic_stack/
-
 sudo docker-compose up -d
 
 
