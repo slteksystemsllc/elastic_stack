28cea6432bd842b6cb40aaab5d1af5de53eecdc1
Scott Lynch <62553406+slteksystemsllc@users.noreply.github.com>
2020-10-14 14:44:25 -0400

    Update initialize.sh

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -5,23 +5,6 @@ git clone https://github.com/slteksystemsllc/elastic_stack.git
 
 sudo chown 1000:1000 -R /opt/elastic_stack/
 
-cp -f /opt/elastic_stack/docker-compose.yml.example /opt/elastic_stack/docker-compose.yml
-cp -f /opt/elastic_stack/curator/example/* /opt/elastic_stack/curator/
-cp -f /opt/elastic_stack/cron/custom-cron.example /opt/elastic_stack/cron/custom-cron
-cp -f /opt/elastic_stack/elastalert/config.yaml.example /opt/elastic_stack/elastalert/config.yaml
-if [ ! -d /opt/elastic_stack/elastalert/rules ];
-then
-  mkdir /opt/elastic_stack/elastalert/rules
-fi
-
-
-mkdir /opt/logstash_configs/
-cp -r /opt/elastic_stack/logstash/logstash_configs/* /opt/logstash_configs/
-mkdir -p /usr/share/logstash/data/
-mkdir -p /usr/share/logstash/config/
-cp -f /opt/elastic_stack/logstash/pipelines.yml.example  /usr/share/logstash/config/pipelines.yml
-
-
 mkdir -p /etc/nsm/rules/
 mkdir -p /opt/elastic_stack/elastalert/rules
 mkdir -p /opt/logstash_configs/
@@ -29,6 +12,15 @@ mkdir -p /opt/domain_stats2/data
 mkdir -p /usr/share/logstash/config/
 mkdir -p /usr/share/logstash/configs/
 mkdir -p /usr/share/logstash/data/
+
+cp -f /opt/elastic_stack/docker-compose.yml.example /opt/elastic_stack/docker-compose.yml
+cp -f /opt/elastic_stack/curator/example/* /opt/elastic_stack/curator/
+cp -f /opt/elastic_stack/cron/custom-cron.example /opt/elastic_stack/cron/custom-cron
+cp -f /opt/elastic_stack/elastalert/config.yaml.example /opt/elastic_stack/elastalert/config.yaml
+cp -r /opt/elastic_stack/logstash/logstash_configs/* /opt/logstash_configs/
+cp -f /opt/elastic_stack/logstash/pipelines.yml.example /usr/share/logstash/config/pipelines.yml
+
+
 
 cd /opt/elastic_stack
 
