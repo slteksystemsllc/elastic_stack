9e49783a9a55ed477cded0be1a1e3302450d1908
Scott Lynch <62553406+slteksystemsllc@users.noreply.github.com>
2020-10-14 16:40:13 -0400

    Update initialize.sh

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -6,34 +6,15 @@ git clone https://github.com/slteksystemsllc/elastic_stack.git
 sudo chown 1000:1000 -R /opt/elastic_stack/
 
 mkdir -p /etc/nsm/rules/
-mkdir -p /opt/elastic_stack/elastalert/rules
 mkdir -p /opt/logstash_configs/
-mkdir -p /opt/domain_stats2/data
 mkdir -p /usr/share/logstash/config/
 mkdir -p /usr/share/logstash/configs/
 mkdir -p /usr/share/logstash/data/
 
-mkdir -p /etc/elastalert/
-mkdir -p /opt/elastalert/
-mkdir -p /opt/elastalert-server/config/
-mkdir -p /opt/elastalert/rules/
-mkdir -p /opt/elastalert/rules_templates/
-
-cp -f /opt/elastic_stack/elastalert/example_rules/* /opt/elastalert/rules/
-cp -f /opt/elastic_stack/elastalert/config.yaml.example /opt/elastalert/config.yaml
-cp -f /opt/elastic_stack/elastalert/config/config.json /opt/elastalert-server/config/config.json
-cp -f /opt/elastic_stack/elastalert/rules /elastalert/rules:
-
- 
-
 cp -f /opt/elastic_stack/docker-compose.yml.example /opt/elastic_stack/docker-compose.yml
-cp -f /opt/elastic_stack/curator/example/* /opt/elastic_stack/curator/
 cp -f /opt/elastic_stack/cron/custom-cron.example /opt/elastic_stack/cron/custom-cron
-cp -f /opt/elastic_stack/elastalert/config.yaml.example /opt/elastic_stack/elastalert/config.yaml
 cp -r /opt/elastic_stack/logstash/logstash_configs/* /opt/logstash_configs/
 cp -f /opt/elastic_stack/logstash/pipelines.yml.example /usr/share/logstash/config/pipelines.yml
-
-
 
 cd /opt/elastic_stack
 
