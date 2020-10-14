9fe0a41c16c22dddd92eff775a0d3fac3828e782
Scott Lynch <62553406+slteksystemsllc@users.noreply.github.com>
2020-10-14 17:10:41 -0400

    Update initialize.sh

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -13,7 +13,7 @@ mkdir -p /usr/share/logstash/data/
 
 cp -f /opt/elastic_stack/docker-compose.yml.example /opt/elastic_stack/docker-compose.yml
 cp -f /opt/elastic_stack/cron/custom-cron.example /opt/elastic_stack/cron/custom-cron
-cp -r /opt/elastic_stack/logstash/logstash_configs/* /opt/logstash_configs/
+cp -f /opt/elastic_stack/logstash/logstash_configs/* /opt/logstash_configs/
 cp -f /opt/elastic_stack/logstash/pipelines.yml.example /usr/share/logstash/config/pipelines.yml
 
 cd /opt/elastic_stack
