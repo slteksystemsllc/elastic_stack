525b675bedf947fae1c4713f3b1a603c352ce384
Scott Lynch <62553406+slteksystemsllc@users.noreply.github.com>
2020-10-14 14:02:23 -0400

    Update initialize.sh

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -13,24 +13,22 @@ if [ ! -d /opt/elastic_stack/elastalert/rules ];
 then
   mkdir /opt/elastic_stack/elastalert/rules
 fi
-mkdir /opt/elastic_stack/elastalert/rules
-cp -f /opt/elastic_stack/elastalert/example_rules/* /opt/elastic_stack/elastalert/rules/
-cp -f /opt/elastic_stack/logstash/pipelines.yml.example /opt/elastic_stack/logstash/pipelines.yml
 
-mkdir -p /usr/share/logstash/config/
-
-cp -f /opt/elastic_stack/logstash/pipelines.yml /usr/share/logstash/config/
 
 mkdir /opt/logstash_configs/
 cp -r /opt/elastic_stack/logstash/logstash_configs/* /opt/logstash_configs/
+mkdir -p /usr/share/logstash/data/
+mkdir -p /usr/share/logstash/config/
+cp -f /opt/elastic_stack/logstash/pipelines.yml /usr/share/logstash/config/
 
 mkdir -p /etc/nsm/rules/
-cp -r /opt/elastic_stack/logstash/rules/* /etc/nsm/rules/
-
-mkdir /usr/share/logstash/data/ 
+mkdir -p /opt/elastic_stack/elastalert/rules
+mkdir -p /opt/logstash_configs/
+mkdir -p /opt/domain_stats2/data
+mkdir -p /usr/share/logstash/config/
+mkdir -p /usr/share/logstash/configs/
+mkdir -p /usr/share/logstash/data/
 
 cd /opt/elastic_stack
 
 docker-compose up --no-start
-
-
